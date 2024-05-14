a57ae0706c5f6527321c37bc932d96cd7873678e
Frederik Rønnevig <frederik.ronnevig@entur.org>
2024-05-14 12:49:09 +0200

    feature: removing security temporary until application shall call other applications

--- src/main/kotlin/no/entur/shared/mobility/to/ref/Application.kt
+++ src/main/kotlin/no/entur/shared/mobility/to/ref/Application.kt
@@ -1,9 +1,13 @@
 package no.entur.shared.mobility.to.ref
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(
+    exclude = [SecurityAutoConfiguration::class, ManagementWebSecurityAutoConfiguration::class],
+)
 class Application
 
 fun main(args: Array<String>) {
