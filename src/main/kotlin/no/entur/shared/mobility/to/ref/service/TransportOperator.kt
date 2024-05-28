5afc3880daecb9d9b61214256f2b4d459ec4a2de
Marcus Goplen <39118923+Goplen@users.noreply.github.com>
2024-05-28 15:48:51 +0200

    ETU-48891: Using valid OperatorId (#33)

--- src/main/kotlin/no/entur/shared/mobility/to/ref/service/TransportOperator.kt
+++ src/main/kotlin/no/entur/shared/mobility/to/ref/service/TransportOperator.kt
@@ -1,7 +1,7 @@
 package no.entur.shared.mobility.to.ref.service
 
 object TransportOperator {
-    const val SCOOTER_OPERATOR = "scooter_operator"
+    const val SCOOTER_OPERATOR = "YTI:Operator:Tier"
     const val BIKE_OPERATOR = "bike_operator"
     const val ALL_IMPLEMENTING_OPERATOR = "all_implementing_operator"
 }
