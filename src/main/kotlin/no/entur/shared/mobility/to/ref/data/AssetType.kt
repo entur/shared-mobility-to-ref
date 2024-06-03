8f7ef5abeec8cf1f3e529669a094338d978cca68
Marcus Goplen <marcus.rosland.goplen@entur.org>
2024-06-03 12:28:06 +0200

    ETU-49013: Removed unused data from responses

--- src/main/kotlin/no/entur/shared/mobility/to/ref/data/AssetType.kt
+++ src/main/kotlin/no/entur/shared/mobility/to/ref/data/AssetType.kt
@@ -3,28 +3,9 @@ package no.entur.shared.mobility.to.ref.data
 import no.entur.shared.mobility.to.ref.dto.AssetClass
 import no.entur.shared.mobility.to.ref.dto.AssetType
 import no.entur.shared.mobility.to.ref.dto.ConditionDeposit
-import no.entur.shared.mobility.to.ref.dto.ConditionPayWhenFinished
-import no.entur.shared.mobility.to.ref.dto.ConditionPostponedCommit
-import no.entur.shared.mobility.to.ref.dto.ConditionRequireBookingData
-import no.entur.shared.mobility.to.ref.dto.ConditionRequireEvidence
 import no.entur.shared.mobility.to.ref.dto.ConditionRequireOffboardingSteps
-import no.entur.shared.mobility.to.ref.dto.ConditionRequireOnboardingSteps
-import no.entur.shared.mobility.to.ref.dto.ConditionRequirePausingSteps
-import no.entur.shared.mobility.to.ref.dto.ConditionRequireResumingSteps
-import no.entur.shared.mobility.to.ref.dto.ConditionReturnArea
-import no.entur.shared.mobility.to.ref.dto.ConditionUpfrontPayment
-import no.entur.shared.mobility.to.ref.dto.Coordinates
-import no.entur.shared.mobility.to.ref.dto.Day
-import no.entur.shared.mobility.to.ref.dto.GeoJsonLine
-import no.entur.shared.mobility.to.ref.dto.GeoJsonPoint
-import no.entur.shared.mobility.to.ref.dto.GeoJsonPolygon
 import no.entur.shared.mobility.to.ref.dto.OffBoardingStep
-import no.entur.shared.mobility.to.ref.dto.OnBoardingStep
-import no.entur.shared.mobility.to.ref.dto.PausingStep
-import no.entur.shared.mobility.to.ref.dto.ResumingStep
-import no.entur.shared.mobility.to.ref.dto.SystemHours
 import no.entur.shared.mobility.to.ref.dto.SystemPricingPlan
-import java.time.OffsetDateTime
 
 val assetType
     get() =
@@ -67,131 +48,17 @@ val conditions
                 vatRate = 21F,
                 vatCountryCode = "NO",
             ),
-            ConditionPayWhenFinished(
-                conditionType = "ConditionPayWhenFinished",
-                id = "deposit50eu",
-            ),
-            ConditionPostponedCommit(
-                conditionType = "ConditionPostponedCommit",
-                id = "deposit50eu",
-                ultimateResponseTime = OffsetDateTime.now(),
-            ),
-            ConditionRequireBookingData(
-                conditionType = "ConditionRequireBookingData",
-                id = "deposit50eu",
-                requiredFields =
-                    listOf(
-                        ConditionRequireBookingData.RequiredFields.FROM_ADDRESS,
-                    ),
-                claims =
-                    listOf(
-                        "string",
-                    ),
-            ),
-            ConditionReturnArea(
-                conditionType = "ConditionReturnArea",
-                id = "deposit50eu",
-                stationId = "string",
-                returnArea =
-                    GeoJsonPolygon(
-                        listOf(
-                            GeoJsonLine(
-                                listOf(
-                                    GeoJsonPoint(listOf(1F, 1F)),
-                                    GeoJsonPoint(listOf(0F, 1F)),
-                                    GeoJsonPoint(listOf(0F, 0F)),
-                                    GeoJsonPoint(listOf(1F, 0F)),
-                                    GeoJsonPoint(listOf(1F, 1F)),
-                                ),
-                            ),
-                        ),
-                    ),
-                coordinates =
-                    Coordinates(
-                        lng = 6.169639F,
-                        lat = 52.25327F,
-                        alt = 0F,
-                    ),
-                returnHours =
-                    listOf(
-                        SystemHours(
-                            userType = SystemHours.UserType.MEMBER,
-                            stationId = "string",
-                            regionId = "string",
-                            startTime = "string",
-                            endTime = "string",
-                            days =
-                                listOf(
-                                    Day.MON,
-                                ),
-                        ),
-                    ),
-            ),
-            ConditionUpfrontPayment(
-                conditionType = "ConditionUpfrontPayment",
-                id = "deposit50eu",
-            ),
-            ConditionRequireEvidence(
-                conditionType = "ConditionRequireEvidence",
-                id = "deposit50eu",
-                evidenceTypes =
-                    listOf(
-                        ConditionRequireEvidence.EvidenceTypes.PARKED,
-                    ),
-            ),
-            ConditionRequireOnboardingSteps(
-                conditionType = "ConditionRequireOnboardingSteps",
-                id = "deposit50eu",
-                steps =
-                    listOf(
-                        OnBoardingStep(
-                            type = OnBoardingStep.Type.URL,
-                            url = "string",
-                            goal = OnBoardingStep.Goal.INSTRUCTIONS,
-                            text = "string",
-                            action = OnBoardingStep.Action.SEND_PREPARE,
-                        ),
-                    ),
-            ),
             ConditionRequireOffboardingSteps(
                 conditionType = "ConditionRequireOffboardingSteps",
                 id = "deposit50eu",
                 steps =
                     listOf(
                         OffBoardingStep(
-                            type = OffBoardingStep.Type.URL,
-                            url = "string",
+                            type = OffBoardingStep.Type.PLAIN_TEXT,
                             goal = OffBoardingStep.Goal.INSTRUCTIONS,
-                            text = "string",
-                            action = OffBoardingStep.Action.SEND_START_FINISHING,
-                        ),
-                    ),
-            ),
-            ConditionRequirePausingSteps(
-                conditionType = "ConditionRequirePausingSteps",
-                id = "deposit50eu",
-                steps =
-                    listOf(
-                        PausingStep(
-                            type = PausingStep.Type.URL,
-                            url = "string",
-                            goal = PausingStep.Goal.INSTRUCTIONS,
-                            text = "string",
-                            action = PausingStep.Action.SEND_PAUSE,
-                        ),
-                    ),
-            ),
-            ConditionRequireResumingSteps(
-                conditionType = "ConditionRequireResumingSteps",
-                id = "deposit50eu",
-                steps =
-                    listOf(
-                        ResumingStep(
-                            type = ResumingStep.Type.URL,
-                            url = "string",
-                            goal = ResumingStep.Goal.INSTRUCTIONS,
-                            text = "string",
-                            action = ResumingStep.Action.UNLOCK_ASSET,
+                            text = "Take a picture of the parked mobility to finish the trip",
+                            showTime = OffBoardingStep.ShowTime.START_FINISHING,
+                            action = OffBoardingStep.Action.SEND_EVIDENCE_PARKED,
                         ),
                     ),
             ),
--- src/main/kotlin/no/entur/shared/mobility/to/ref/data/Booking.kt
+++ src/main/kotlin/no/entur/shared/mobility/to/ref/data/Booking.kt
@@ -1,23 +1,12 @@
 package no.entur.shared.mobility.to.ref.data
 
 import no.entur.shared.mobility.to.ref.dto.Address
-import no.entur.shared.mobility.to.ref.dto.AssetClass
 import no.entur.shared.mobility.to.ref.dto.Booking
-import no.entur.shared.mobility.to.ref.dto.BookingAllOfExtraData
 import no.entur.shared.mobility.to.ref.dto.BookingState
-import no.entur.shared.mobility.to.ref.dto.Card
-import no.entur.shared.mobility.to.ref.dto.CardType
 import no.entur.shared.mobility.to.ref.dto.Coordinates
 import no.entur.shared.mobility.to.ref.dto.Customer
 import no.entur.shared.mobility.to.ref.dto.Fare
-import no.entur.shared.mobility.to.ref.dto.Information
-import no.entur.shared.mobility.to.ref.dto.License
-import no.entur.shared.mobility.to.ref.dto.LicenseType
-import no.entur.shared.mobility.to.ref.dto.Phone
 import no.entur.shared.mobility.to.ref.dto.Place
-import no.entur.shared.mobility.to.ref.dto.Requirements
-import no.entur.shared.mobility.to.ref.dto.StopReference
-import java.time.LocalDate
 import java.time.OffsetDateTime
 import java.util.UUID
 
@@ -26,10 +15,7 @@ val booking
         Booking(
             id = UUID.randomUUID().toString(),
             from = place,
-            callbackUrl = "string",
-            to = place,
-            customer = customer,
-            extraInfo = mapOf("additionalProp1" to "prop"),
+            customer = Customer(id = "A0-123456"),
             state = BookingState.CONFIRMED,
             legs = listOf(leg),
             pricing = fare,
@@ -37,47 +23,18 @@ val booking
             arrivalTime = OffsetDateTime.now().plusMinutes(12),
             actualDepartureTime = OffsetDateTime.now(),
             actualArrivalTime = OffsetDateTime.now().plusMinutes(12),
-            mainAssetType = assetType,
-            userCommunication =
-                listOf(
-                    Information(
-                        type = Information.Type.URL,
-                        url = "string",
-                        goal = Information.Goal.INSTRUCTIONS,
-                        text = "string",
-                    ),
-                ),
-            memo = "string",
-            extraData =
-                BookingAllOfExtraData(
-                    safeWaitTime = 0,
-                    maxWaitTime = 0,
-                    safeTravelTime = 0,
-                    maxTravelTime = 0,
-                ).apply { put("additionalProp1", "prop") },
         )
 
 val place
     get() =
         Place(
             name = "string",
-            stopReference =
-                listOf(
-                    StopReference(
-                        type = StopReference.Type.GTFS_STOP_ID,
-                        id = "string",
-                        country = "NO",
-                    ),
-                ),
-            stationId = "string",
             coordinates =
                 Coordinates(
                     lng = 6.169639F,
                     lat = 52.25327F,
                     alt = 0F,
                 ),
-            physicalAddress = address,
-            extraInfo = mapOf("additionalProp1" to "prop"),
         )
 
 val address
@@ -96,79 +53,6 @@ val address
             country = "NO",
         )
 
-val customer
-    get() =
-        Customer(
-            isValidated = true,
-            age = 0,
-            referenceNumber = "string",
-            cardTypes =
-                listOf(
-                    CardType(
-                        type = CardType.Type.ID,
-                        subType = "string",
-                        assetClass = AssetClass.AIR,
-                        acceptors = listOf("string"),
-                    ),
-                ),
-            licenseTypes =
-                listOf(
-                    LicenseType(
-                        assetClass = AssetClass.AIR,
-                        issuingCountry = "NO",
-                    ),
-                ),
-            requirements = requirements,
-            knownIdentifier = "string",
-            knownIdentifierProvider = "string",
-            id = "A0-123456",
-            travelerReference = "string",
-            initials = "string",
-            firstName = "John",
-            lastName = "Doe",
-            middleName = "von",
-            prefix = "string",
-            postfix = "string",
-            phones =
-                listOf(
-                    Phone(
-                        preferred = true,
-                        number = "+31-48934758 or +(0075)-834923384 or 020 1234 1234",
-                        kind = Phone.Kind.LANDLINE,
-                        type = Phone.Type.PRIVATE,
-                    ),
-                ),
-            email = "string",
-            birthDate = LocalDate.parse("2024-02-16"),
-            address = address,
-            photo = null,
-            cards =
-                listOf(
-                    Card(
-                        type = Card.Type.ID,
-                        subType = "string",
-                        assetClass = AssetClass.AIR,
-                        acceptors = listOf("string"),
-                        cardDescription = "string",
-                        cardNumber = "string",
-                        cardAdditionalNumber = "string",
-                        validUntil = LocalDate.parse("2024-02-16"),
-                        country = "NO",
-                    ),
-                ),
-            licenses =
-                listOf(
-                    License(
-                        assetClass = AssetClass.AIR,
-                        issuingCountry = "NO",
-                        number = "1287948792",
-                        licenseCode = "D4",
-                        validUntil = LocalDate.parse("2024-02-16"),
-                    ),
-                ),
-            extraInfo = mapOf("additionalProp1" to "prop"),
-        )
-
 val fare
     get() =
         Fare(
@@ -177,10 +61,3 @@ val fare
             propertyClass = "string",
             parts = listOf(farePartFlex),
         )
-
-val requirements
-    get() =
-        Requirements(
-            abilities = listOf(requirement),
-            bringAlong = listOf(requirement),
-        ).apply { put("additionalProp1", "prop") }
