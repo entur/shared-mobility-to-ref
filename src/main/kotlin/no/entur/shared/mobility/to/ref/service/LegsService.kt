9dfb347f33587facf1792a456ed5007165a91d20
Marcus Goplen <marcus.rosland.goplen@entur.org>
2024-07-04 08:18:05 +0200

    ETU-49927: Added correct return values in legsIdEventsPost for new operators

--- src/main/kotlin/no/entur/shared/mobility/to/ref/service/LegsService.kt
+++ src/main/kotlin/no/entur/shared/mobility/to/ref/service/LegsService.kt
@@ -94,37 +94,38 @@ class LegsService {
         addressedTo: String?,
         legEvent: LegEvent,
     ): Leg {
-        val leg: Leg =
-            leg.copy(
-                id = id,
-                state =
-                    when (legEvent.event) {
-                        LegEvent.Event.PREPARE -> LegState.PREPARING
-                        LegEvent.Event.ASSIGN_ASSET -> LegState.NOT_STARTED
-                        LegEvent.Event.SET_IN_USE -> LegState.IN_USE
-                        LegEvent.Event.PAUSE -> LegState.PAUSED
-                        LegEvent.Event.OPEN_TRUNK -> LegState.IN_USE
-                        LegEvent.Event.START_FINISHING -> LegState.FINISHING
-                        LegEvent.Event.FINISH -> LegState.FINISHED
-                        LegEvent.Event.TIME_EXTEND -> LegState.IN_USE
-                        LegEvent.Event.TIME_POSTPONE -> LegState.NOT_STARTED
-                        LegEvent.Event.CANCEL -> LegState.CANCELLED
-                    },
-                actualArrivalTime =
-                    when (legEvent.event) {
-                        LegEvent.Event.START_FINISHING -> OffsetDateTime.now().plusMinutes(12)
-                        LegEvent.Event.FINISH -> OffsetDateTime.now().plusMinutes(12)
-                        else -> null
-                    },
-                asset = legEvent.asset,
-            )
-
-        return when (addressedTo) {
-            SCOOTER_OPERATOR -> leg
-            BIKE_OPERATOR -> leg
-            ALL_IMPLEMENTING_OPERATOR -> leg
-            else -> throw NotImplementedError()
-        }
+        val leg =
+            when (addressedTo) {
+                SCOOTER_OPERATOR_NO_DEPOSIT -> legWithoutDeposit
+                SCOOTER_OPERATOR_DEPOSIT_HIGHER_THAN_TOTAL_PRICE -> legWithHighDepositAmount
+                SCOOTER_OPERATOR -> leg
+                BIKE_OPERATOR -> leg
+                ALL_IMPLEMENTING_OPERATOR -> leg
+                else -> throw NotImplementedError()
+            }
+        return leg.copy(
+            id = id,
+            state =
+                when (legEvent.event) {
+                    LegEvent.Event.PREPARE -> LegState.PREPARING
+                    LegEvent.Event.ASSIGN_ASSET -> LegState.NOT_STARTED
+                    LegEvent.Event.SET_IN_USE -> LegState.IN_USE
+                    LegEvent.Event.PAUSE -> LegState.PAUSED
+                    LegEvent.Event.OPEN_TRUNK -> LegState.IN_USE
+                    LegEvent.Event.START_FINISHING -> LegState.FINISHING
+                    LegEvent.Event.FINISH -> LegState.FINISHED
+                    LegEvent.Event.TIME_EXTEND -> LegState.IN_USE
+                    LegEvent.Event.TIME_POSTPONE -> LegState.NOT_STARTED
+                    LegEvent.Event.CANCEL -> LegState.CANCELLED
+                },
+            actualArrivalTime =
+                when (legEvent.event) {
+                    LegEvent.Event.START_FINISHING -> OffsetDateTime.now().plusMinutes(12)
+                    LegEvent.Event.FINISH -> OffsetDateTime.now().plusMinutes(12)
+                    else -> null
+                },
+            asset = legEvent.asset,
+        )
     }
 
     fun legsIdGet(
