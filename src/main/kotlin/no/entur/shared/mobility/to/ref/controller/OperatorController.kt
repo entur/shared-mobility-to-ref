5cc36805786b1770cd6434ce2d1c6057468ac45c
Marcus Goplen <39118923+Goplen@users.noreply.github.com>
2024-09-16 14:44:46 +0200

    ETU-51762: Removed /api from controllers (#70)

--- src/main/kotlin/no/entur/shared/mobility/to/ref/controller/OperatorController.kt
+++ src/main/kotlin/no/entur/shared/mobility/to/ref/controller/OperatorController.kt
@@ -24,13 +24,11 @@ import no.entur.shared.mobility.to.ref.service.OperatorService
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestHeader
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @Validated
-@RequestMapping("\${api.base-path}")
 class OperatorController(private val operatorService: OperatorService) {
     @Operation(
         hidden = true,
