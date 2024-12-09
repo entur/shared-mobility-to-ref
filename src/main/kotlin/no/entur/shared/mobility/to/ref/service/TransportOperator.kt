6a84ac69867a0b704c622682166bf30fad2b0633
Bent Lorentzen <bent.lorentzen@entur.org>
2024-12-09 12:08:15 +0100

    ETU-54340: Fikset code space så det matcher feed.

--- src/main/kotlin/no/entur/shared/mobility/to/ref/service/TransportOperator.kt
+++ src/main/kotlin/no/entur/shared/mobility/to/ref/service/TransportOperator.kt
@@ -1,9 +1,9 @@
 package no.entur.shared.mobility.to.ref.service
 
 object TransportOperator {
-    const val SCOOTER_OPERATOR = "YTI:Operator:Altair"
-    const val SCOOTER_OPERATOR_NO_DEPOSIT = "YVO:Operator:evie"
-    const val SCOOTER_OPERATOR_DEPOSIT_HIGHER_THAN_TOTAL_PRICE = "YBO:Operator:ezio"
+    const val SCOOTER_OPERATOR = "YAL:Operator:Altair"
+    const val SCOOTER_OPERATOR_NO_DEPOSIT = "YEV:Operator:evie"
+    const val SCOOTER_OPERATOR_DEPOSIT_HIGHER_THAN_TOTAL_PRICE = "YEZ:Operator:ezio"
     const val BIKE_OPERATOR = "bike_operator"
     const val ALL_IMPLEMENTING_OPERATOR = "all_implementing_operator"
 }
