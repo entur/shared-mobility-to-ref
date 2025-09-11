447d456d85fbf322c1c3aa664decd89a5ce70dfe
Bent  Lorentzen <bent.lorentzen@entur.org>
2025-09-11 14:10:46 +0200

    ETU-62008 - Lagt til client for shared-mobility-router (#139)

--- src/main/kotlin/no/entur/shared/mobility/to/ref/config/TransportOperator.kt
+++ src/main/kotlin/no/entur/shared/mobility/to/ref/config/TransportOperator.kt
@@ -6,6 +6,6 @@ object TransportOperator {
     const val SCOOTER_OPERATOR_3 = "YBA:Operator:Basim"
     const val SCOOTER_OPERATOR_NO_DEPOSIT = "YEV:Operator:Evie"
     const val SCOOTER_OPERATOR_DEPOSIT_HIGHER_THAN_TOTAL_PRICE = "YEZ:Operator:Ezio"
-    const val BIKE_OPERATOR = "bike_operator"
-    const val ALL_IMPLEMENTING_OPERATOR = "all_implementing_operator"
+    const val BIKE_OPERATOR = "BOP:Operator:bike_operator"
+    const val ALL_IMPLEMENTING_OPERATOR = "ALL:Operator:all_implementing_operator"
 }
