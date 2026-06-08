e236fd3e468796a4d4d9538738af4a14366320f7
Bent  Lorentzen <bent.lorentzen@entur.org>
2026-06-08 08:26:25 +0200

    ETU-68995 - Oppdradert til gardel 9.5.1, JDK 25 og SB4 (#193)

--- src/main/kotlin/no/entur/shared/mobility/to/ref/Application.kt
+++ src/main/kotlin/no/entur/shared/mobility/to/ref/Application.kt
@@ -1,15 +1,11 @@
 package no.entur.shared.mobility.to.ref
 
-import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication(
-    exclude = [SecurityAutoConfiguration::class, ManagementWebSecurityAutoConfiguration::class],
-)
+@SpringBootApplication
 class Application
 
 fun main(args: Array<String>) {
-    runApplication<no.entur.shared.mobility.to.ref.Application>(*args)
+    runApplication<Application>(*args)
 }
